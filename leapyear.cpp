cbdf34b38a1d66e20aea55f80fd0890d3fa93226
priyadharshiniap <32993324+priyadharshiniap@users.noreply.github.com>
2017-12-28 09:45:34 +0530

    Create leapyear.cpp

---
+++ leapyear.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+cout<<"enter the year";
+cin>>a;
+if(a%4==0)
+{
+  cout<<"leap year";
+}
+else
+cout<<"not a leap year";
+getch();
+}
